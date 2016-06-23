ad9aab0aa33aabc333e6f79a3e00b6444099dba7
5oldman <5oldman@gmail.com>
2016-06-23 16:23:06 +0900

    FTP maven depository delete

--- src/main/java/com/baron/member/service/BookServiceImpl.java
+++ src/main/java/com/baron/member/service/BookServiceImpl.java
@@ -22,9 +22,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
